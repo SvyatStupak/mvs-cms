3f6cd6979d9dc45af72854e8df42443c6e81c6b5
Sviat Stupak <48685109+SvyatStupak@users.noreply.github.com>
2022-09-13 09:15:13 +0300

    Finish 1 Lesson

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$section = $_GET['section'] ?? 'home';
+
+
+if ($section === 'about-us') {
+    include 'controller/AboutUsPage.php';
+} else if ($section === 'contact') {
+    include 'controller/ContactPage.php';
+} else {
+    include 'controller/HomePage.php';
+}
+
+
+
